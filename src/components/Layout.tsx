e8aaa41794c8b9f3cd808e8a41fba1e37e2f104e
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-02 19:31:22 +0900

    💄 layout 마크업

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+type TProps = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: TProps) {
+  return (
+    <div className="flex justify-center text-center my-10">
+      <div className="w-[500px] lg:w-[800px]">{children}</div>
+    </div>
+  )
+}
