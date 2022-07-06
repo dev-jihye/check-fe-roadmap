2bd6c80fb11065649d905929a8339b49b7c2bd23
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-06 14:40:21 +0900

    💄 ProgressBar 컴포넌트 생성

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,8 @@
+export default function ProgressBar() {
+  return (
+    <div>
+      <div className="w-full h-4 bg-violet-200 rounded-md"></div>
+      <span className="block w-full mt-2 text-center text-sm">1 / 20</span>
+    </div>
+  );
+}
