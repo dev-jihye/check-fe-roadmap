34af0058f4851c2abdf933a74ad589bd0ed476a6
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:51:12 +0900

    💄 Selection 마크업 수정 및 Props 추가

--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -1,11 +1,15 @@
 type TProps = {
   title: string;
+  onClick: () => void;
 };
 
-export default function Selection(props: TProps) {
+export default function Selection({title, onClick}: TProps) {
   return (
-    <button className="bg-sky-100 rounded-md w-full p-4 my-2">
-      {props.title}
+    <button
+      onClick={onClick}
+      className="bg-[#2e3074] active:bg-[#6979bb] rounded-md w-full p-4 my-2"
+    >
+      {title}
     </button>
   );
 }
