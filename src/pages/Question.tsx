cd5c4418b531d71fc095abfbbddddaa25fba6221
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:52:20 +0900

    💄 Question 마크업 수정

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,22 +1,52 @@
+import {useEffect, useState} from 'react';
 import ProgressBar from '../components/ProgressBar';
 import Selection from '../components/Selection';
+import Models from '../data';
+
+const {CategoryModel, QuestionModel} = Models();
+
+const questions = QuestionModel.map((question) => {
+  return {
+    ...question,
+    category: CategoryModel.find(
+      (category) => category.id === question.categoryId,
+    ),
+  };
+});
 
 export default function Question() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [question, setQuestion] = useState(questions[currentIndex]);
+  console.log(questions, 'q');
+
+  const handleClick = () => {
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  useEffect(() => {
+    setQuestion(questions[currentIndex]);
+  }, [currentIndex]);
+
   return (
-    <div className="border">
-      <div className="inline-block py-2 px-4 rounded-full bg-violet-300 mb-10 text-xs">
-        <span>HTML</span>
-        <span className="pl-2">1 / 5</span>
+    <div>
+      <div className="inline-block py-2 px-4 rounded-full border-2 border-[#17d1ff] text-[#17d1ff] mb-10 text-xs">
+        <span>{question.category.title}</span>
       </div>
-      <ProgressBar />
+      <ProgressBar
+        currentPage={currentIndex + 1}
+        totalPage={questions.length}
+      />
       <div>
         <h2 className="text-center my-20 text-lg sm:text-xl">
-          시맨틱 태그가 무엇인지 알고 설명할 수 있다
+          {question.title}
         </h2>
-        <Selection title="전혀 모른다." />
-        <Selection title="들어본 적은 있다." />
-        <Selection title="개념은 아는데 자세한 설명은 생략한다.." />
-        <Selection title="설명까지 SSAP! 가능!" />
+        <Selection onClick={handleClick} title="전혀 모른다." />
+        <Selection onClick={handleClick} title="들어본 적은 있다." />
+        <Selection
+          onClick={handleClick}
+          title="개념은 아는데 자세한 설명은 생략한다.."
+        />
+        <Selection onClick={handleClick} title="설명까지 SSAP! 가능!" />
       </div>
     </div>
   );
