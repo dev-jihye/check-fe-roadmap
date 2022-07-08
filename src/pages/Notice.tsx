d0a88bd3c429c839076585f831ad0ec4ca44235f
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:52:08 +0900

    💄 Notice 마크업 수정

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,13 +1,14 @@
 import {SpeakerphoneIcon} from '@heroicons/react/outline';
+import {Link} from 'react-router-dom';
 import StartBtn from '../components/StartBtn';
 
 export default function Notice() {
   return (
-    <div className="border px-4 sm:px-20">
-      <SpeakerphoneIcon className="w-6 h-6 mx-auto mb-4" />
+    <div className="px-4 sm:px-20">
+      <SpeakerphoneIcon className="w-6 h-6 sm:w-8 sm:h-8 mx-auto my-4" />
       <p className="text-sm sm:text-base mb-6">
-        현재의 본인의 위치를 알기 위한 자가진단 테스트로 결과에 성실하게
-        답변해주시고 결과에 이어지는 로드맵은 권장사항입니다.
+        현재 본인의 위치를 알기 위한 자가진단 테스트로 결과에 성실하게
+        답변해주세요. 결과에 이어지는 로드맵은 권장사항입니다.
       </p>
       <p className="text-sm sm:text-base">
         총 문항은 30개로 구성되어 있습니다.
@@ -20,7 +21,9 @@ export default function Notice() {
         <li>Tools 5문항</li>
       </ul>
       <div className="flex justify-center items-center">
-        <StartBtn />
+        <Link to="/question">
+          <StartBtn />
+        </Link>
       </div>
     </div>
   );
