2543321ca16846f1e9526fe3a08a17b9cae80fd8
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:50:11 +0900

    💄 layout 마크업 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,8 @@ type TProps = {
 
 export default function Layout({children}: TProps) {
   return (
-    <div className="flex justify-center items-center w-full h-full px-4 py-10">
-      <div className="w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl">
+    <div className="flex justify-center items-center w-full min-h-screen px-4 py-10 bg-[#170a34]">
+      <div className="w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl text-[#f9f7e3]">
         {children}
       </div>
     </div>
