25a567f26d31d2cf6b999855db618c022f1bd2bf
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:51:29 +0900

    💄 ShareBtn 마크업 수정

--- src/components/ShareBtn.tsx
+++ src/components/ShareBtn.tsx
@@ -4,7 +4,9 @@ export default function ShareBtn() {
   return (
     <div className="flex flex-col justify-center items-center">
       <button className="mb-2 text-sm">공유하기</button>
-      <ShareIcon className="w-6 h-6" />
+      <div className="bg-[#3178b8] p-2 rounded-full cursor-pointer">
+        <ShareIcon className="w-6 h-6" />
+      </div>
     </div>
   );
 }
