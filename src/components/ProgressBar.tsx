fad96df475a8643b36a4efa0d7c2d57ef7c7eed2
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-08 17:50:38 +0900

    💄 Progess bar 마크업 수정

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,8 +1,15 @@
-export default function ProgressBar() {
+type TProps = {
+  totalPage: number;
+  currentPage: number;
+};
+
+export default function ProgressBar({totalPage, currentPage}: TProps) {
   return (
     <div>
-      <div className="w-full h-4 bg-violet-200 rounded-md"></div>
-      <span className="block w-full mt-2 text-center text-sm">1 / 20</span>
+      <div className="w-full h-4 bg-[#e9e069] rounded-md"></div>
+      <span className="block w-full mt-2 text-center text-sm">
+        {currentPage} / {totalPage}
+      </span>
     </div>
   );
 }
