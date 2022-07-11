0c65da4316b6374279ac31c0311c3ecad73190b1
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-11 17:55:57 +0900

    ✨ Question 기능 구현

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -2,40 +2,60 @@ import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import ProgressBar from '../components/ProgressBar';
 import Selection from '../components/Selection';
-import Models from '../data';
+import Models, {TQuestionModel, TCategoryModel} from '../data';
 
 const {CategoryModel, QuestionModel} = Models();
 
-const questions = QuestionModel.map((question) => {
-  return {
-    ...question,
-    category: CategoryModel.find(
-      (category) => category.id === question.categoryId,
-    ),
-  };
-});
+type TQuestionModelInLS = TQuestionModel & {
+  category: TCategoryModel | undefined;
+  score?: number;
+};
 
 export default function Question() {
+  const [questions, setQuestions] = useState<TQuestionModelInLS[]>(
+    QuestionModel.map((question) => {
+      return {
+        ...question,
+        category: CategoryModel.find(
+          (category) => category.id === question.categoryId,
+        ),
+      };
+    }),
+  );
   const [currentIndex, setCurrentIndex] = useState(0);
   const [question, setQuestion] = useState(questions[currentIndex]);
   const navigate = useNavigate();
   console.log(questions, 'q');
 
-  const handleClick = () => {
-    setCurrentIndex(currentIndex + 1);
-    if (currentIndex === questions.length - 1) {
-      navigate('/result');
-    }
-  };
-
   useEffect(() => {
     setQuestion(questions[currentIndex]);
-  }, [currentIndex]);
+  }, [currentIndex, questions]);
 
+  const handleClick = (score: number) => {
+    if (currentIndex === questions.length - 1) {
+      localStorage.setItem('questions', JSON.stringify(questions));
+      navigate('/result');
+    }
+
+    setQuestions(
+      questions.map((question, index) => {
+        if (index === currentIndex) {
+          return {
+            ...question,
+            score,
+          };
+        } else {
+          return question;
+        }
+      }),
+    );
+
+    setCurrentIndex(currentIndex + 1);
+  };
   return (
     <div>
       <div className="inline-block py-2 px-4 rounded-full border-2 border-[#17d1ff] text-[#17d1ff] mb-10 text-xs">
-        <span>{question.category.title}</span>
+        <span>{question.category?.title || '카테고리'}</span>
       </div>
       <ProgressBar
         currentPage={currentIndex + 1}
@@ -45,13 +65,30 @@ export default function Question() {
         <h2 className="text-center my-20 text-lg sm:text-xl">
           {question.title}
         </h2>
-        <Selection onClick={handleClick} title="전혀 모른다." />
-        <Selection onClick={handleClick} title="들어본 적은 있다." />
         <Selection
-          onClick={handleClick}
+          onClick={() => {
+            handleClick(1);
+          }}
+          title="전혀 모른다."
+        />
+        <Selection
+          onClick={() => {
+            handleClick(2);
+          }}
+          title="들어본 적은 있다."
+        />
+        <Selection
+          onClick={() => {
+            handleClick(3);
+          }}
           title="개념은 아는데 자세한 설명은 생략한다.."
         />
-        <Selection onClick={handleClick} title="설명까지 SSAP! 가능!" />
+        <Selection
+          onClick={() => {
+            handleClick(4);
+          }}
+          title="설명까지 SSAP! 가능!"
+        />
       </div>
     </div>
   );
