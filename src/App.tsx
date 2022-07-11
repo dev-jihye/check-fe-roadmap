2fd016691c84b97541a2951a4ae68f65ec46d9a9
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-11 23:23:03 +0900

    ✨ 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Layout from './components/Layout';
+import Loading from './components/Loading';
+import Home from './pages/Home';
+import Notice from './pages/Notice';
+import Question from './pages/Question';
+import Result from './pages/Result';
+import Roadmap from './pages/Roadmap';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
-  return <div className="text-2xl">hello</div>;
+  return (
+    <Layout>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/roadmap" element={<Roadmap />} />
+        </Routes>
+      </HashRouter>
+    </Layout>
+  );
 }
 
 export default App;
