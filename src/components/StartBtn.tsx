c1c7551c0aafccb880e5a86868ba86b3fd503141
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-13 22:51:57 +0900

    ✨ StartBtn props 받기

--- src/components/StartBtn.tsx
+++ src/components/StartBtn.tsx
@@ -1,7 +1,11 @@
-export default function StartBtn() {
+type TProps = {
+  text?: string;
+};
+
+export default function StartBtn({text = '테스트 시작하기'}: TProps) {
   return (
     <button className="text-sm sm:text-base px-5 py-4 rounded-md bg-[#e9e069] text-gray-900">
-      테스트 시작하기
+      {text}
     </button>
   );
 }
