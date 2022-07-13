339127538943d80058f888412abd8d4a91cc3459
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-13 22:45:23 +0900

    💄 Selection 디자인 변경

--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -7,7 +7,7 @@ export default function Selection({title, onClick}: TProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-[#2e3074] active:bg-[#6979bb] rounded-md w-full p-4 my-2"
+      className="bg-[#5d4893] active:bg-[#3f2d7a] rounded-md w-full p-4 my-2"
     >
       {title}
     </button>
