0896e1e01ee90065b99ad86082f657319115be98
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-13 23:44:15 +0900

    ✨ Result 마크업 및 기능 구현

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,39 +1,74 @@
-import roadImg from '../assets/running.png';
-import {MapIcon} from '@heroicons/react/outline';
-import ShareBtn from '../components/ShareBtn';
+import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import roadImg from '../assets/running.png';
+import ShareBtn from '../components/ShareBtn';
+import StartBtn from '../components/StartBtn';
+import Models from '../data';
+import {TQuestionModelInLS} from '../types/model';
 
 export default function Result() {
+  const {CategoryModel, QuestionModel} = Models();
+
+  const [result, setResult] = useState<TQuestionModelInLS[]>([]);
+
+  useEffect(() => {
+    const allQuestions = QuestionModel.map((question) => {
+      return {
+        ...question,
+        category: CategoryModel.find(
+          (category) => category.id === question.categoryId,
+        ),
+        score: 1,
+      };
+    });
+    const questionsFromLS = localStorage.getItem('questions');
+    if (questionsFromLS) {
+      const questions = JSON.parse(questionsFromLS);
+      setResult(questions);
+    } else {
+      setResult(allQuestions);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center mb-16">
         <h2 className="text-lg sm:text-2xl text-center mb-16">
-          당신은 프론트엔드 개발자까지 60% 왔습니다.
+          이 부분을 더 공부하면 좋을 것 같아요!
         </h2>
         <img className="w-96" src={roadImg} alt="road" />
       </div>
-      <div className="w-full px-4 sm:w-[32rem] mb-14">
+      <div className="w-full sm:w-[32rem] mb-10">
         <div className="mb-6">
-          <p className="text-lg sm:text-xl">💪 나의 강점</p>
-          <p className="mt-2">HTML, CSS, Javascript, React, CS 기초 지식</p>
-        </div>
-        <div>
-          <p className="text-lg sm:text-xl">✍ 나의 약점</p>
-          <p className="mt-2">React, CS 기초 지식</p>
-        </div>
-        <div className="flex justify-center items-center">
-          <a
-            href="#"
-            className="flex justify-center items-center mt-10 bg-[#e9e069] text-gray-900 px-5 py-4 rounded-md text-sm sm:text-base"
-          >
-            <MapIcon className="w-5 h-5 mr-2" />
-            <Link to="/roadmap" className="text-sm sm:text-base">
-              추천 로드맵 보러가기
-            </Link>
-          </a>
+          {result
+            .filter((question: any) => question.score <= 3)
+            .map((question: any) => (
+              <div
+                key={question.id}
+                className="flex items-center justify-between bg-[#282044] text-[#eee] p-3 sm:p-4 my-4 rounded-md space-x-4"
+              >
+                <p className="text-sm sm:text-base">{question.title}</p>
+                <a
+                  href={question.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm sm:text-base shrink-0"
+                >
+                  <span className="py-1 px-3 bg-[#7559bf] text-white text-sm rounded-md mr-2 tracking-wide">
+                    Link
+                  </span>
+                </a>
+              </div>
+            ))}
         </div>
       </div>
-      <ShareBtn />
+      <Link to="/" className="mb-10">
+        <StartBtn text="테스트 다시하기" />
+      </Link>
+      <div className="mb-10">
+        <ShareBtn url={window.location.origin} text="테스트 공유하기" />
+      </div>
     </div>
   );
 }
