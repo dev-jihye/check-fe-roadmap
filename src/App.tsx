6048a8a44ae5f91e45b58bcdacf0cbf8fc404e2b
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-13 23:43:24 +0900

    🔥 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import Layout from './components/Layout';
-import Loading from './components/Loading';
 import Home from './pages/Home';
 import Notice from './pages/Notice';
 import Question from './pages/Question';
 import Result from './pages/Result';
-import Roadmap from './pages/Roadmap';
 import {HashRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -17,7 +14,6 @@ function App() {
           <Route path="/notice" element={<Notice />} />
           <Route path="/question" element={<Question />} />
           <Route path="/result" element={<Result />} />
-          <Route path="/roadmap" element={<Roadmap />} />
         </Routes>
       </HashRouter>
     </Layout>
