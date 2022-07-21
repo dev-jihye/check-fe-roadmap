e6c3f4772faaeb3cfcbfbb6ae352397ac89de3a2
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-07-21 21:43:49 +0900

    ✨ 다 알고 있다고 체크했을 때 result 분기

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import roadImg from '../assets/running.png';
+import finish from '../assets/finish-line.png';
 import ShareBtn from '../components/ShareBtn';
 import StartBtn from '../components/StartBtn';
 import Models from '../data';
@@ -8,8 +9,9 @@ import {TQuestionModelInLS} from '../types/model';
 
 export default function Result() {
   const {CategoryModel, QuestionModel} = Models();
-
   const [result, setResult] = useState<TQuestionModelInLS[]>([]);
+  const isPerfect =
+    result.filter((question) => question.score === 4).length === result.length;
 
   useEffect(() => {
     const allQuestions = QuestionModel.map((question) => {
@@ -33,36 +35,75 @@ export default function Result() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center items-center mb-16">
-        <h2 className="text-lg sm:text-2xl text-center mb-16">
-          이 부분을 더 공부하면 좋을 것 같아요!
-        </h2>
-        <img className="w-96" src={roadImg} alt="road" />
-      </div>
-      <div className="w-full sm:w-[32rem] mb-10">
-        <div className="mb-6">
-          {result
-            .filter((question: any) => question.score <= 3)
-            .map((question: any) => (
-              <div
-                key={question.id}
-                className="flex items-center justify-between bg-[#282044] text-[#eee] p-3 sm:p-4 my-4 rounded-md space-x-4"
-              >
-                <p className="text-sm sm:text-base">{question.title}</p>
-                <a
-                  href={question.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sm sm:text-base shrink-0"
-                >
-                  <span className="py-1 px-3 bg-[#7559bf] text-white text-sm rounded-md mr-2 tracking-wide">
-                    Link
-                  </span>
-                </a>
-              </div>
-            ))}
+      {isPerfect ? (
+        <div>
+          <div className="flex flex-col justify-center items-center mb-16">
+            <h2 className="text-lg sm:text-2xl text-center mb-16">
+              이론은 이미 척척 박사🧑‍🎓 이제는 실전 차례!
+            </h2>
+            <img className="w-96" src={finish} alt="road" />
+            <span className="text-xs mt-5">Image from iconscout</span>
+          </div>
+          <div className="w-full sm:w-[32rem] mb-10">
+            <div className="mb-6">
+              {result
+                .filter((question: any) => question.score <= 3)
+                .map((question: any) => (
+                  <div
+                    key={question.id}
+                    className="flex items-center justify-between bg-[#282044] text-[#eee] p-3 sm:p-4 my-4 rounded-md space-x-4"
+                  >
+                    <p className="text-sm sm:text-base">{question.title}</p>
+                    <a
+                      href={question.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm sm:text-base shrink-0"
+                    >
+                      <span className="py-1 px-3 bg-[#7559bf] text-white text-sm rounded-md mr-2 tracking-wide">
+                        Link
+                      </span>
+                    </a>
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className="flex flex-col justify-center items-center mb-16">
+            <h2 className="text-lg sm:text-2xl text-center mb-16">
+              이 부분을 더 공부하면 좋을 것 같아요!
+            </h2>
+            <img className="w-96" src={roadImg} alt="road" />
+            <span className="text-xs mt-5">Image from kindpng</span>
+          </div>
+          <div className="w-full sm:w-[32rem] mb-10">
+            <div className="mb-6">
+              {result
+                .filter((question: any) => question.score <= 3)
+                .map((question: any) => (
+                  <div
+                    key={question.id}
+                    className="flex items-center justify-between bg-[#282044] text-[#eee] p-3 sm:p-4 my-4 rounded-md space-x-4"
+                  >
+                    <p className="text-sm sm:text-base">{question.title}</p>
+                    <a
+                      href={question.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-sm sm:text-base shrink-0"
+                    >
+                      <span className="py-1 px-3 bg-[#7559bf] text-white text-sm rounded-md mr-2 tracking-wide">
+                        Link
+                      </span>
+                    </a>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      )}
       <Link to="/" className="mb-10">
         <StartBtn text="테스트 다시하기" />
       </Link>
